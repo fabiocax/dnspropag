5cfd8d7fa98262f2542321f7af4358188b3b8f7c
bsd0x <gmdutra.bsd@gmail.com>
2020-06-16 11:59:27 -0300

    Update DNS server list

--- dnspropag.py
+++ dnspropag.py
@@ -12,18 +12,20 @@ def print_resolver(dns_server_location, dns_data):
 @click.option('--dnstype', nargs=1)
 def main(domain, dnstype):
     dns_servers = collections.OrderedDict()
-    
+
     dns_servers['Australia'] = "203.89.226.24"
     dns_servers["Brazil"] = "200.181.14.3"
-    dns_servers["Canada"] = "72.139.50.226"
+    dns_servers["Cloudflare"] = "1.1.1.1"
     dns_servers["Chile"] = "200.27.137.187"
     dns_servers["China"] = "202.46.34.75"
     dns_servers["France"] = "188.165.220.211"
     dns_servers["Germany"] = "78.46.231.162"
     dns_servers["Google"] = "8.8.8.8"
     dns_servers["Google"] = "8.8.4.4"
-    dns_servers["Cloudflare"] = "1.1.1.1"
-    
+    dns_servers["Japan"] = "101.110.33.227"
+    dns_servers["Africa"] = "41.208.16.161"
+    dns_servers["USA"] = "199.45.32.43"
+
     dns_resolver = dns.resolver.Resolver()
 
     for dns_server_location, dns_server in zip(dns_servers.keys(), \
@@ -35,7 +37,7 @@ def main(domain, dnstype):
 
             [print_resolver(dns_server_location, dns_data) \
                 for dns_data in dns_query]
-            
+
         except:
             print_resolver(dns_server_location, 'X')        
 
